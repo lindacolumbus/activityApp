b2ea1dcdf328103341d3951d41d372a3d6cfae7f
lindacolumbus <lindadcolumbus@gmail.com>
2021-12-23 22:10:26 -0500

    added successful API calls for Unsplash and Giphy based on Bored API returne data

--- script.js
+++ script.js
@@ -1,35 +1,12 @@
 const app = {};
 
 app.activityURL = 'http://www.boredapi.com/api/activity/';
-app.apiKey = `StpMdwFhlOwrb4d-Ed7owQCq6bx4QCDQv_8wgN4x3tc`;
+app.imageApiKey = `StpMdwFhlOwrb4d-Ed7owQCq6bx4QCDQv_8wgN4x3tc`;
+app.gifApiKey = 'WZ5Osv7W0JPH1duyxCbrQWP789FKAXs3';
 app.imageURL = `https://api.unsplash.com/search/photos`;
+app.gifURL = 'https://api.giphy.com/v1/gifs/search';
 
 let participants = 1;
-
-// app.getActivity = (activity) => {
-//     const url = new URL('http://www.boredapi.com/api/activity/');
-//     url.search = new URLSearchParams({
-//         participants: `${participants}`
-//     })
-//     fetch(url)
-//         .then((response) => {
-//             if (response.ok) {
-//                 return response.json();
-//             } else { 
-//                 throw new Error(response.statusText)
-//             }
-//         })
-//         .then((jsonResponse) => {
-//             console.log(jsonResponse)
-//             // weatherApp.displayTodaysData(jsonResponse);
-//         })
-//         .catch((error) => {
-//             // Modal error display
-//             if (error.message === `Not Found` || `Bad Request`) {
-//                 weatherApp.modalErrorHandling();
-//             }
-//         })
-// }
 
 async function getActivity() {
     // Bored API activity data
@@ -40,39 +17,42 @@ async function getActivity() {
     const activityDataObject = await fetch(url);
     const activity = await activityDataObject.json();
     console.log(activity)
-    // const activityString = activity.activity.split(' ');
+    const activityString = activity.activity.split(' ');
 
-    // const longWord = activityString.filter(word => (word.length >= 5 && !word.includes(`'`)));
-
-    // const imageSearch = longWord.join(',');
-    // console.log(imageSearch)
-
-    const imageSearch = activity.activity.split(' ').join(',');
+    const longWord = activityString.filter(word => (word.length >= 4 && !word.includes(`'`)));
+    const imageSearch = longWord.join(',');
     console.log(imageSearch)
 
-
+    const gifSearch = longWord.join(' ');
+    console.log(gifSearch)
 
     // Unsplash API image data, based on activity data returned
     const imageURL = new URL(app.imageURL);
     imageURL.search = new URLSearchParams({
-        client_id: app.apiKey,
+        client_id: app.imageApiKey,
         query: imageSearch,
-        per_page: 1
+        per_page: 1,
+        orientation: 'portrait'
         })
     const imageDataObject = await fetch(imageURL);
     const image = await imageDataObject.json();
     console.log(image.results[0].urls.full)
 
+    // Giphy API data, based on activity data returned
+    const gifURL = new URL(app.gifURL);
+    gifURL.search = new URLSearchParams({
+        api_key: app.gifApiKey,
+        q: gifSearch,
+        limit: 1,
+        rating: 'pg',
+        lang: 'en'
+        })
+    const gifDataObject = await fetch(gifURL);
+    const gif = await gifDataObject.json();
+    console.log(gif.data[0].embed_url)
+
   return activity;
 }
-
-
-// async function getActivity(activity) {
-//   const response = await fetch(`http://www.boredapi.com/api/activity/?participants=1/`)
-//   const data = await response.json();
-//   console.log(data)
-//   return data;
-// }
 
 app.init = () => {
     getActivity();
