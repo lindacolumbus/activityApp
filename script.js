b7697d5f6718fc71fadc073220f23867138c3546
lindacolumbus <lindadcolumbus@gmail.com>
2021-12-28 22:05:07 -0500

    fixed order of events so that activity h2 loads once radio input is selected, changed and submitted

--- script.js
+++ script.js
@@ -6,73 +6,75 @@ app.gifApiKey = 'WZ5Osv7W0JPH1duyxCbrQWP789FKAXs3';
 app.imageURL = `https://api.unsplash.com/search/photos`;
 app.gifURL = 'https://api.giphy.com/v1/gifs/search';
 
-async function getActivity() {
-    // user's radio input selection
-    const radio = document.getElementsByName('numParticipants');
+// user's radio input selection
+app.radio = document.getElementsByName('numParticipants');
+app.radioChoice = document.querySelector('.radioInput button');
 
-    for (i = 0; i < radio.length; i++) {
-        if (radio[i].checked) {
-            participants = radio[i].value;
-            console.log(participants)};
-    }
+app.startEventListener = () => {
+    app.radioChoice.addEventListener('click', e => {
+        // loop through the radio input values
+        for (i = 0; i < app.radio.length; i++) {
+            // once a radio input is selected, call the APIs
+            if (app.radio[i].checked) {
+                participants = app.radio[i].value;
 
-    // Bored API activity data
-    const url = new URL(app.activityURL);
-    url.search = new URLSearchParams({
-        participants: `${participants}`
-        })
-    const activityDataObject = await fetch(url);
-    // activity recommendation data
-    const activity = await activityDataObject.json();
+                async function getActivity() {
+                    // Bored API activity data
+                    const url = new URL(app.activityURL);
+                    url.search = new URLSearchParams({
+                        participants: `${participants}`
+                    })
+                    const activityDataObject = await fetch(url);
 
-    const radioChoice = document.querySelector('.radioInput button');
-    console.log(radioChoice)
-    radioChoice.addEventListener('click', e => {
-        console.log(e)
-        if (e) {
-            const results = document.querySelector('.activityRecommendation');
-            results.innerText = activity.activity;
+                    // activity recommendation data
+                    const activity = await activityDataObject.json();
+                    console.log(activity)
+                    // h2 activity recommendation string
+                    const results = document.querySelector('.activityRecommendation');
+                    results.innerText = activity.activity;
+                
+                    // format activity recommendation to use for the two API calls
+                    const activityString = activity.activity.split(' ');
+                    const longWord = activityString.filter(word => (word.length >= 3 && !word.includes(`'`)));
+                    const imageSearch = longWord.join(',');
+                    const gifSearch = longWord.join(' ');
+                
+                    // Unsplash API image data, based on activity data returned
+                    const imageURL = new URL(app.imageURL);
+                    imageURL.search = new URLSearchParams({
+                        client_id: app.imageApiKey,
+                        query: imageSearch,
+                        per_page: 1,
+                        orientation: 'portrait'
+                        })
+                    const imageDataObject = await fetch(imageURL);
+                    const image = await imageDataObject.json();
+                    const imageSrc = await image.results[0].urls.full;
+                    const altDescription = await image.results[0].alt_description;
+                    console.log(imageSrc)
+                
+                    // Giphy API data, based on activity data returned
+                    const gifURL = new URL(app.gifURL);
+                    gifURL.search = new URLSearchParams({
+                        api_key: app.gifApiKey,
+                        q: gifSearch,
+                        limit: 1,
+                        rating: 'pg',
+                        lang: 'en'
+                        })
+                    const gifDataObject = await fetch(gifURL);
+                    const gif = await gifDataObject.json();
+                    const gifSrc = await gif.data[0].embed_url;
+                    console.log(gifSrc)
+                }
+                getActivity()
+            }
         }
     })
-    console.log(activity.activity)
-
-    const activityString = activity.activity.split(' ');
-
-    const longWord = activityString.filter(word => (word.length >= 3 && !word.includes(`'`)));
-    const imageSearch = longWord.join(',');
-    const gifSearch = longWord.join(' ');
-
-    // Unsplash API image data, based on activity data returned
-    const imageURL = new URL(app.imageURL);
-    imageURL.search = new URLSearchParams({
-        client_id: app.imageApiKey,
-        query: imageSearch,
-        per_page: 1,
-        orientation: 'portrait'
-        })
-    const imageDataObject = await fetch(imageURL);
-    const image = await imageDataObject.json();
-    const imageSrc = await image.results[0].urls.full;
-    const altDescription = await image.results[0].alt_description;
-    console.log(imageSrc)
-
-    // Giphy API data, based on activity data returned
-    const gifURL = new URL(app.gifURL);
-    gifURL.search = new URLSearchParams({
-        api_key: app.gifApiKey,
-        q: gifSearch,
-        limit: 1,
-        rating: 'pg',
-        lang: 'en'
-        })
-    const gifDataObject = await fetch(gifURL);
-    const gif = await gifDataObject.json();
-    const gifSrc = await gif.data[0].embed_url;
-    console.log(gifSrc)
 }
 
 app.init = () => {
-    getActivity();
+    app.startEventListener();
 }
 
 app.init();
